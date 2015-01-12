fe6df303614fa1b27f8fe2aab094bfed8aed6bd1
Erik van Paassen <erik@eveoh.nl>
2015-01-12 15:42:19 +0100

    Rewrite ExchangeServiceBase, HttpWebRequest, etc. to reuse HttpClient.
    Also added close() method to ExchangeServiceBase to be able to shut down
    the HTTP connection manager cleanly.

--- src/main/java/microsoft/exchange/webservices/data/AutodiscoverService.java
+++ src/main/java/microsoft/exchange/webservices/data/AutodiscoverService.java
@@ -333,7 +333,7 @@ public final class AutodiscoverService extends ExchangeServiceBase implements
     HttpWebRequest request = null;
 
     try {
-      request = new HttpClientWebRequest(this.getHttpConnectionManager());
+      request = new HttpClientWebRequest(httpClient, httpContext);
 
       try {
         request.setUrl(URI.create(url).toURL());
@@ -342,29 +342,15 @@ public final class AutodiscoverService extends ExchangeServiceBase implements
         throw new ServiceLocalException(strErr);
       }
 
-      request.setAllowAutoRedirect(false);
-      request.setPreAuthenticate(false);
       request.setRequestMethod("GET");
-      request.setUseDefaultCredentials(this.getUseDefaultCredentials());
-      if (!this.getUseDefaultCredentials()) {
-        ExchangeCredentials serviceCredentials = this.getCredentials();
-        if (null == serviceCredentials) {
-          throw new ServiceLocalException(Strings.CredentialsRequired);
-        }
-        // Make sure that credentials have been authenticated if required
-        serviceCredentials.preAuthenticate();
+      request.setAllowAutoRedirect(false);
 
-        // Apply credentials to the request
-        serviceCredentials.prepareWebRequest(request);
-      }
+      // Do NOT allow authentication as this single request will be made over plain HTTP.
+      request.setAllowAuthentication(false);
 
-      try {
-        request.prepareAsyncConnection();
-      } catch (Exception e) {
-        log.warn("Error while preparing asynchronous connection.", e);
-        traceMessage(TraceFlags.AutodiscoverConfiguration, "No Autodiscover redirection URL was returned.");
-        return null;
-      }
+      prepareCredentials();
+
+      request.prepareConnection();
 
       OutParam<URI> outParam = new OutParam<URI>();
       if (tryGetRedirectionResponse(request, outParam)) {
@@ -1475,7 +1461,7 @@ public final class AutodiscoverService extends ExchangeServiceBase implements
 
       HttpWebRequest request = null;
       try {
-        request = new HttpClientWebRequest(this.getHttpConnectionManager());
+        request = new HttpClientWebRequest(httpClient, httpContext);
 
         try {
           request.setUrl(autoDiscoverUrl.toURL());
@@ -1489,25 +1475,9 @@ public final class AutodiscoverService extends ExchangeServiceBase implements
         request.setPreAuthenticate(false);
         request.setUseDefaultCredentials(this.getUseDefaultCredentials());
 
-        if (!this.getUseDefaultCredentials()) {
-          ExchangeCredentials serviceCredentials = this.getCredentials();
-          if (null == serviceCredentials) {
-            throw new ServiceLocalException(Strings.CredentialsRequired);
-          }
+        prepareCredentials();
 
-          // Make sure that credentials have been authenticated if required
-          serviceCredentials.preAuthenticate();
-
-          // Apply credentials to the request
-          serviceCredentials.prepareWebRequest(request);
-        }
-
-        try {
-          request.prepareAsyncConnection();
-        } catch (Exception e) {
-          log.warn("Error while preparing asynchronous connection.", e);
-          return false;
-        }
+        request.prepareConnection();
 
         URI redirectUrl;
         OutParam<URI> outParam = new OutParam<URI>();
